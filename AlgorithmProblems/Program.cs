4f337e5c3734fead3a05f463a2124504757b4071
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-05 23:55:57 +0530

    [Rahmath]Add: Binary Search The Word

---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmProblems;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To Algorithm Programs");
+        bool end = true;
+        Console.WriteLine("1.BinarySearch\n2.End the Program");
+        while (end == true)
+        {
+            Console.WriteLine("Take an option to execute");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BasicAlgorithms basic = new BasicAlgorithms();
+                    basic.Binary();
+                    break;
+                case 2:
+                    end = false;
+                    break;
+                default:
+                    Console.WriteLine("Enter Proper Option To Execute");
+                    break;
+            }
+        }
+    }
+}

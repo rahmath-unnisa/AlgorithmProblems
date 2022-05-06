80f3b3b47e151358693cac3ad4cd2559fce1ab64
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-06 09:38:32 +0530

    [Rahmath]Add: Performing Bubble Sort

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.WriteLine("                                   WELCOME TO ALGORITHM PROGRAMS                                   ");
         bool check = true;
         const string path = @"D:\Practice Problems\AlgorithmProblems\AlgorithmProblems\AlgorithmProblems\Data.txt";
-        Console.WriteLine("1.Binary Search\n2. Insertion Sort \n3.Exit");
+        Console.WriteLine("1.Binary Search\n2. Insertion Sort \n3. Bubble Sort \n4.Exit");
         while (check)
         {
             Console.Write("\nEnter an option to execute :");
@@ -25,6 +25,10 @@ class Program
                 case 2:
                     InsertionSort sort = new InsertionSort();
                     sort.Sort();
+                    break;
+                case 3:
+                    BubbleSort bubble = new BubbleSort();
+                    bubble.Sort();
                     break;
                 default:
                     check = false;
